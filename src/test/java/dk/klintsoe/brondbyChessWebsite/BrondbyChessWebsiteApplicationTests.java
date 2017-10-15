4f8ca68e237070ff7055a8d1134fbbc1ab96de40
klintsoe <klintsoe@gmail.com>
2017-10-15 20:17:32 +0200

    added

--- src/test/java/dk/klintsoe/brondbyChessWebsite/BrondbyChessWebsiteApplicationTests.java
+++ src/test/java/dk/klintsoe/brondbyChessWebsite/BrondbyChessWebsiteApplicationTests.java
@@ -2,15 +2,31 @@ package dk.klintsoe.brondbyChessWebsite;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class BrondbyChessWebsiteApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BrondbyChessWebsiteApplicationTests {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testOne() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print())
+                .andExpect(status().isOk())
+        //.andExpect(jsonPath("$.name").value("Søren"))
+        //	.andExpect(jsonPath("$.playerId").value("1"))
+        ;
+    }
+}
