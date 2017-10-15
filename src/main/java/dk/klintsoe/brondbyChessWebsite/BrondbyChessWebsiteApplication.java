0ba10bd0209d1650ab47fdf66651ef73a495bdf1
klintsoe <klintsoe@gmail.com>
2017-10-15 16:14:46 +0200

    added

---
+++ src/main/java/dk/klintsoe/brondbyChessWebsite/BrondbyChessWebsiteApplication.java
@@ -0,0 +1,12 @@
+package dk.klintsoe.brondbyChessWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrondbyChessWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrondbyChessWebsiteApplication.class, args);
+	}
+}
